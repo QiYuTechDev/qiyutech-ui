805a3da1aa6a8bbe3cd006456176b206affd17d1
dev <dev@qiyutech.tech>
2021-10-14 11:49:41 +0800

    should set env property

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     preset: 'ts-jest',
 
+    // Jest Doc for testEnvironment
+    // https://jestjs.io/zh-Hans/docs/configuration#testenvironment-string
+    //
     // fix document is not defined
     // https://github.com/testing-library/react-testing-library/issues/422#issuecomment-518007141
     testEnvironment: 'jsdom',
