3bd5c35cd11397bb7d75d8c083bfc475f2813b43
dev <dev@qiyutech.tech>
2021-10-14 11:48:03 +0800

    jsdom works :)

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     preset: 'ts-jest',
 
-    testEnvironment: 'node',
+    // fix document is not defined
+    // https://github.com/testing-library/react-testing-library/issues/422#issuecomment-518007141
+    testEnvironment: 'jsdom',
 
     setupFilesAfterEnv: ['./jest-setup.ts'],
 };
